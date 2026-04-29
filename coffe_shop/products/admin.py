31dbcb62e404d8d3e4bbb414ced9235063c397c4
Fabio Nehuel Montiel <montielfabio91@gmail.com>
2026-04-29 00:07:33 -0300

    agregue e hice modificaciones en el admin

--- coffe_shop/products/admin.py
+++ coffe_shop/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+#clase para personalizar la forma en que se muestran los productos en el admin de Django
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['name', 'price', 'available'] #para mostrar el nombre, precio y disponibilidad del producto en la lista de productos en el admin
+    search_fields = ['name', 'description'] #para buscar por nombre o descripción del producto
+
+admin.site.register(Product, ProductAdmin)
