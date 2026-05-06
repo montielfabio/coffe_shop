db6c60817f320501311eb51935c433b9ede1cd1a
Fabio Nehuel Montiel <montielfabio91@gmail.com>
2026-05-06 20:31:06 -0300

    se corrigio models orders

--- orders/models.py
+++ orders/models.py
@@ -17,7 +17,7 @@ class OrderProduct(models.Model):
     order = models.ForeignKey(Order, related_name='order_products', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name} for Order {self.order.id}" 
